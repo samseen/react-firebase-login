230e2de0dcd3ab0eb3ca4e9ed3be47d3cdd2dc9b
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-28 18:22:38 +0100

    Created the login handling function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import fire from './fire'
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [hasAccount, setHasAccount] = useState(false);
+
+  const handleLogin = () => {
+    fire
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(err => {
+        switch(err.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-found":
+            setEmailError(err.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(err.message);
+            break;
+        }
+      })
+  }
+
   return (
     <div className="App">
       <h1>Hello World</h1>
