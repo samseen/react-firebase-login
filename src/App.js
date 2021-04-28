8a91f7eedbe96cb43b7e1ae0a49a25641e37f69a
Samson Akanbi <samson.akanbi@outlook.com>
2021-04-28 21:20:05 +0100

    Passed the HandleLogout parameter to the hero component.

--- src/App.js
+++ src/App.js
@@ -81,6 +81,7 @@ function App() {
   return (
     <div className="App">
       <Login email={email} setEmail={setEmail} password={password} setPassword={setPassword} handleLogin={handleLogin} handleSignup={handleSignup} hasAccount={hasAccount} setHasAccount={setHasAccount} emailError={emailError} passwordError={passwordError} />
+      <Hero handleLogout={handleLogout} />
     </div>
   );
 }
